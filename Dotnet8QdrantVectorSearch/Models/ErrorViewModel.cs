42772539a8bf153b3766397659bd6408b2a5cefe
Weber Yang <weberyanglalala@gmail.com>
2025-04-10 16:12:03 +0800

    project init

---
+++ Dotnet8QdrantVectorSearch/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Dotnet8QdrantVectorSearch.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
